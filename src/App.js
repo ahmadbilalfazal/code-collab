f1c8f75fa7ff04c6eae3c9019ecc760f21782559
ahmadbilalfazal <ahmadbilalfazal7@gmail.com>
2025-04-23 14:21:08 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import io from 'socket.io-client';
+
+const socket = io('http://YOUR_LOCAL_IP:4000');
 
 function App() {
+  const [code, setCode] = useState('');
+  const [docId] = useState('default-doc');
+  const docIdRef = useRef(docId);
+
+  useEffect(() => {
+    socket.emit('join', docIdRef.current);
+    socket.on('change', (newCode) => {
+      if (newCode !== code) setCode(newCode);
+    });
+    socket.on('load', (initialContent) => {
+      setCode(initialContent);
+    });
+    return () => {
+      socket.off('change');
+      socket.off('load');
+    };
+  }, []);
+
+  const handleChange = (value) => {
+    setCode(value);
+    socket.emit('change', value, docIdRef.current);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
+      <h1>Collaborative Code Editor</h1>
+      <CodeMirror
+        value={code}
+        height="500px"
+        extensions={[javascript()]}
+        onChange={handleChange}
+      />
+      <p>Open this in another browser to see real-time collaboration!</p>
     </div>
   );
 }
